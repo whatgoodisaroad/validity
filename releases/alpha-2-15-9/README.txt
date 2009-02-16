eb950e5caaee76939ceefa6fdbd14407dff4eade
wyatt.allen0 <wyatt.allen0@fb59f884-fa1c-11dd-9fdb-3debd5008f28>
2009-02-16 00:37:31 +0000

    Added alpha release to the releases directory.

---
+++ releases/alpha-2-15-9/README.txt
@@ -0,0 +1,19 @@
+README for...
+
+  jQuery.validity alpha (v. 0.9.0)
+  http://code.google.com/p/validity
+  Copyright (c) 2009, Wyatt Allen
+  Licenced under the New BSD Licence
+  http://www.opensource.org/licenses/bsd-license.php
+  Revision 3. Feb. 15 2009
+  
+In order to make use of this demo, you'll need to install the actual jQuery 
+library, as it is not included in this archive. The demo page 'index.htm' makes
+reference to 'jquery.js' for the actual library import, so either rename the 
+library that you download or edit the demo page in order to load the correct 
+file.
+
+The demo page explains the use of the validity framework to an extent, but for
+a complete guide, refer to the documentation.
+
+Thanks!
---
+++ releases/alpha-2-15-9/jquery.validity.js
@@ -0,0 +1,259 @@
+﻿/* _ _____                                  _ _     _ _ _         
+  (_)  _  |                                | (_)   | (_) | (alpha)
+   _| | | |_   _  ___ _ __ _   ___   ____ _| |_  __| |_| |_ _   _ 
+  | | | | | | | |/ _ \ '__| | | \ \ / / _` | | |/ _` | | __| | | |
+  | \ \/' / |_| |  __/ |  | |_| |\ V / (_| | | | (_| | | |_| |_| |
+  | |\_/\_\\__,_|\___|_|   \__, (_)_/ \__,_|_|_|\__,_|_|\__|\__, |
+ _/ |                       __/ |                            __/ |
+|__/                       |___/                            |___/ 
+*/
+/*!
+ * jQuery.validity alpha (v. 0.9.0)
+ * http://code.google.com/p/validity
+ *
+ * Copyright (c) 2009, Wyatt Allen
+ * Licenced under the New BSD Licence
+ * http://www.opensource.org/licenses/bsd-license.php
+ *
+ * Revision 3. Feb. 15 2009
+ *
+*/
+(function($) {
+    //// Static Functions and Properties /////////////////
+    jQuery.validity = {
+        // Central location for validity configuration properties.
+        config: {
+            outputMode: "Modal", // [Modal|Summary|None|Custom]
+            outputMethod: null
+        },
+        
+        // Remove all errors on the page.
+        Clear: function(){
+            if($.validity.config.outputMode.toUpperCase() == "MODAL")
+                $('.validity-modal-msg').remove();
+                
+            else if($.validity.config.outputMode.toUpperCase() == "SUMMARY"){
+                $('#validity-summary-output').hide().html('');
+                $('.validity-erroneous').removeClass('validity-erroneous');
+            }
+        }
+    };
+    
+    //// Public Methods /////////////////
+    
+    // Validate whether the field has a value.
+    jQuery.fn.Require = function(msg){
+        var regimen = new Array();
+        
+        regimen['this.value.length > 0'] = msg;
+        
+        return validate(this, regimen);
+    };
+    
+    // Validate whether the field matches a regex.
+    jQuery.fn.Match = function(msg, regex){
+        var regimen = new Array();
+        
+        regimen['/' + regex + '/.test(this.value)'] = msg;
+        
+        return validate(this, regimen);
+    }
+    
+    // Validate that a field has a value, then, if that fails,
+    // validate that it matches a regex.
+    jQuery.fn.RequireThenMatch = function(rmsg, mmsg, regex){
+        var regimen = new Array();
+        
+        regimen['this.value.length > 0']            = rmsg;
+        regimen['/' + regex + '/.test(this.value)'] = mmsg;
+        
+        return validate(this, regimen);
+    }
+    
+    // Validate that all matched elements bear the same values.
+    jQuery.fn.AreEqual = function(msg){
+        if(this.length > 0){
+            var temp = this.get(0).value;
+            var valid = true;
+            var obj = this;
+            
+            this.each(
+                function(){
+                    if(this.value != temp)
+                        valid = false;
+                }
+            );
+            
+            if(!valid)
+                raiseAggregateError(obj, msg);
+            
+            return valid;
+        }
+        
+        else return true;
+    };
+    
+    // Validate that all matched elements bear distinct values.
+    jQuery.fn.AreNotEqual = function(msg){
+        if(this.length > 0){
+            var values = new Array();
+            var obj = this;
+            var valid = true;
+            
+            this.each(
+                function(idx){
+                    for(i in values){
+                        if(this.value != '' && values[i] == this.value)
+                            valid = false;
+                    }
+                    
+                    values[idx] = this.value;
+                }
+            );
+            
+            if(!valid)
+                raiseAggregateError(obj, msg);
+            
+            return valid;
+        }
+        
+        else return true;
+    };
+    
+    // Validate that the numeric sum of all values is equal to a given value.
+    jQuery.fn.Sum = function(msg, sum){
+        if(this.length > 0){            
+            var actual = numericSum(this);
+            
+            var valid = sum == actual;
+            
+            if(!valid)
+                raiseAggregateError(this, msg);
+            
+            return valid;
+        }
+        
+        else 
+            return false;
+    }
+    
+    // Validates an inclusive upper-bound on the numeric sum of the values of all matched elements.
+    jQuery.fn.SumMax = function(msg, max){
+        if(this.length > 0){            
+            var actual = numericSum(this);
+            var valid = max >= actual;
+            
+            if(!valid)
+                raiseAggregateError(this, msg);
+            
+            return valid;
+        }
+        
+        else 
+            return true;
+    }
+    
+    // Validate matched elements based on a user-defined regimen.
+    jQuery.fn.ValidateRegimen = function(regimen){ 
+        return validate(this, regimen); 
+    }
+    
+    //// Private Functions /////////////////
+    
+    // Private function to validate an element based on a 
+    // linear set of conditions.
+    function validate(elem, regimen){
+        var valid = true;
+        
+        elem.each(
+            function() {
+                for(key in regimen){
+                    if(!eval(key)){
+                        raiseError(this, regimen[key]);
+                        valid = false;
+                        break;
+                    }
+                }
+            }
+        );
+        
+        return valid;
+    }
+    
+    // Raise an error appropriately for the element with the message.
+    function raiseError(elem, msg){
+        if($.validity.config.outputMode.toUpperCase() == "MODAL")
+            raiseModalError($(elem), msg);
+            
+        else if($.validity.config.outputMode.toUpperCase() == "SUMMARY")
+            raiseSummaryError($(elem), msg);
+    };
+    
+    // Raise an error with a modal message.
+    function raiseModalError(obj, msg){
+        var off = obj.offset();
+        
+        var computedLeft = off.left + obj.width() + 4;
+        var computedTop = off.top - 10;
+        
+        if($('#validity-msg-' + obj.attr('id')).length == 0)
+            $('#validity-modal-output').append(
+                "<div id='validity-msg-" + obj.attr('id') + "' " + 
+                "class='validity-modal-msg' " + 
+                "style='left: " + computedLeft + "px;top: " + computedTop + "px;'" + 
+                "onclick='$(this).remove()'>" + 
+                msg + "</div>"
+            );
+        else
+            $('#validity-msg-' + obj.attr('id'))
+                .css({ 
+                    left: computedLeft + 'px', 
+                    top: computedTop + 'px' 
+                })
+                .html(msg);
+    };
+    
+    // Display error in a summary output.
+    function raiseSummaryError(obj, msg){
+        pumpToSummary(msg);
+        
+        obj.addClass('validity-erroneous');
+    };
+    
+    // Merely outputs text to the summary.
+    function pumpToSummary(msg){
+        $('#validity-summary-output').show().append('* ' + msg + '<br />');
+    }
+    
+    // Raise a single error for several elements.
+    function raiseAggregateError(obj, msg){
+        if($.validity.config.outputMode.toUpperCase() == "MODAL")
+            raiseAggregateModalError(obj, msg);
+            
+        else if($.validity.config.outputMode.toUpperCase() == "SUMMARY")
+            raiseSummaryError(obj, msg);
+    }
+    
+    // Raise a modal error on the first matched element.
+    function raiseAggregateModalError(obj, msg){
+        if(obj.length > 0)
+            raiseModalError($(obj.get(0)), msg);
+    }
+    
+    // Yield the sum of the values of all fields matched in obj that can be parsed.
+    function numericSum(obj){
+        var accumulator = 0;
+        
+        obj.each(
+            function(){
+                var n = parseFloat(this.value);
+                
+                if(!isNaN(n))
+                    accumulator += n;
+            }
+        );
+        
+        return accumulator;
+    }
+})(jQuery);
+
