6840745c3bcd123da98848c05dbb40028b6e8c88
wyatt.allen0 <wyatt.allen0@fb59f884-fa1c-11dd-9fdb-3debd5008f28>
2009-08-11 19:04:50 +0000

    Fixed code formatting. (Replaced tabs. I'm being OCD about tabs.)

--- jQuery.validity/jquery.validity.js
+++ jQuery.validity/jquery.validity.js
@@ -32,7 +32,7 @@
         
         // Built-in library of format-checking tools for use with the 
         // match validator (as well as the nonHtml validator).
-        patterns: {
+        patterns:{
             integer:/^\d+$/,
             date:function(val) { return !isNaN(Date.parse(val)); },
             email:/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i,
@@ -41,10 +41,10 @@
             number:function(val) { return !isNaN(parseFloat(val)); },
             zip:/^\d{5}(-\d{4})?$/,
             phone:/^([\(]{1}[0-9]{3}[\)]{1}[\.| |\-]{0,1}|^[0-9]{3}[\.|\-| ]?)?[0-9]{3}(\.|\-| )?[0-9]{4}$/,
-			guid:/^(\{{0,1}([0-9a-fA-F]){8}-([0-9a-fA-F]){4}-([0-9a-fA-F]){4}-([0-9a-fA-F]){4}-([0-9a-fA-F]){12}\}{0,1})$/,
-			time12:/^[01][0-9]:[0-6][0-9]?[aApP]\.?[mM]\.?$/,
-			time24:/^(20|21|22|23|[01]\d|\d)(([:][0-5]\d){1,2})$/,
-			
+            guid:/^(\{{0,1}([0-9a-fA-F]){8}-([0-9a-fA-F]){4}-([0-9a-fA-F]){4}-([0-9a-fA-F]){4}-([0-9a-fA-F]){12}\}{0,1})$/,
+            time12:/^[01][0-9]:[0-6][0-9]?[aApP]\.?[mM]\.?$/,
+            time24:/^(20|21|22|23|[01]\d|\d)(([:][0-5]\d){1,2})$/,
+            
             nonHtml:/[^<>]/g
         },
         
