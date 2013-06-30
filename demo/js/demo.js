597d77afd50d2ca7349ec18ae658b5d49482326d
Wyatt Allen <wyatt.allen0@gmail.com>
2013-06-29 22:37:32 -0700

    Live test now encodes pre boxes in the hash

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -1,10 +1,36 @@
 $(function() {
-    
-    $("#form-code-pre").on("keyup", function() {
-        $("#form-content").html(
-            $("#form-code-pre").text()
+    if (location.hash.length) {
+        var json = Base64.decode(
+            location.hash
+                .replace("!", "=")
+                .replace(/^#/, "")
         );
-    });
+        try {
+            var obj = JSON.parse(json);
+            $("#form-code-pre").text(obj.form);
+            $("#validation-code-pre").text(obj.validation);
+        }
+        catch (exc) { }
+    }
+
+    $("#form-code-pre, #validation-code-pre")
+        .on("change keyup", function() {
+            location.hash = Base64.encode(
+                JSON.stringify({
+                    form:$("#form-code-pre").text(),
+                    validation:$("#validation-code-pre").text()
+                })
+            ).replace("=", "!");
+        });
+
+
+    $("#form-code-pre")
+        .on("keyup", function() {
+            $("#form-content").html(
+                $("#form-code-pre").text()
+            );
+        })
+        .trigger("keyup");
 
     
     $("#test-submit")
